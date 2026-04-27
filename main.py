c6f8e9c899df898714480da983008fc0cbd4ceb7
Nicolas Laub <nicolaslaub@Nicolass-MacBook-Air-5.local>
2026-04-27 11:57:16 -0700

    make template generic

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import uvicorn
+
+app = FastAPI ()
+templates = Jinja2Templates(directory='templates')
+
+@app.get('/', response_class=HTMLResponse)
+async def index(request: Request): 
+    return templates.TemplateResponse(
+    request=request,
+    name='index.html'
+    )
+
+@app.get('/login', response_class=HTMLResponse)
+async def index(request: Request): 
+    return templates.TemplateResponse(
+    request=request,
+    name='login.html'
+    )
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host='127.0.0.1', port=8080, reload=True)
